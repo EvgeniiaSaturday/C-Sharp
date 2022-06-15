829f6d40264fa1c022968b4e9afcdbb8f9285edb
Subbotina Evgeniya (Субботина Евгения Владимировна) <subbotina@gloria-jeans.ru>
2022-06-15 20:51:34 +0300

    add task 10

---
+++ task 10/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.Clear();
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int secondDigit = number / 10;
+int twoDigit = secondDigit % 10;
+Console.WriteLine(twoDigit);
+
