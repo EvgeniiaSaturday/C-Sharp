a52694c6d6bf3e0a434a08bc77a1929ac7e37ece
Subbotina Evgeniya (Субботина Евгения Владимировна) <subbotina@gloria-jeans.ru>
2022-06-15 21:43:53 +0300

    add task 15

---
+++ task 15/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+
+Console.Clear();
+Console.Write("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int A =number;
+if (A > 0 && A<8 )
+{
+   if( A==1 || A==2 || A==3 || A==4 || A==5)
+   {
+    Console.WriteLine("нет");
+   }
+    else
+    {
+      Console.WriteLine("да");
+    }
+}
